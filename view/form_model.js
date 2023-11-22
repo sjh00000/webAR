ce67e42feea2a83752b292642eac1ad8a9c2921d
孙佳豪 <1362974876@qq.com>
2023-11-22 23:31:28 +0800

    刷新功能，文字简介

--- view/form_model.js
+++ view/form_model.js
@@ -9,18 +9,10 @@
         nftElement[i].addEventListener('markerFound',(event)=> {
             const scene = document.querySelector('a-scene')
             const id =nowid
-            //const image = document.createElement('a-image')
             const model = document.createElement('a-entity')
-            var modelUrl;
-            //Three.js流动线条模型
-            //const model = document.createElement('a-arline')
-
-            //文字图片
-            //image.setAttribute('src','../image.png')
-            //image.setAttribute('position','0 -2 -10')
-            //image.setAttribute('id','nowimage')
-            //image.setAttribute('width','10')
-            //image.setAttribute('height','3')
+            const image = document.createElement('a-image')
+            var modelUrl
+            var imageUrl
 
             //ajax获取模型
             $.ajax({
@@ -32,6 +24,8 @@
                     var nftId = response[i].id;
                     if(nftId==id){
                         modelUrl=response[i].model_url
+                        imageUrl=response[i].image_url
+                        console.log(imageUrl)
                         //模型设置
                         model.setAttribute('gltf-model', modelUrl)
                         model.setAttribute('position', '0 1 -10')
@@ -45,16 +39,18 @@
                           model.setAttribute('scale','1.5 0.7 1.5')
                         }
                         model.setAttribute('look-controls','enabled:true')
-                        //model.setAttribute('start' , '0 0 -5')
-                        //model.setAttribute('end' , '5 0 -5')
-                        //model.setAttribute('color', '0xff0000')
-                                
-                                
-                        //scene.appendChild(image)
+
+                        //简介图片设置
+                        image.setAttribute('src',imageUrl)
+                        image.setAttribute('position','3 0 -10')
+                        image.setAttribute('id','nowimage')
+                        image.setAttribute('scale','3 3 3')
+
                         // 将model添加到<a-scene>中
                         scene.appendChild(model)
+                        scene.appendChild(image)
                         console.log(model)
-                        //console.log(image)
+                        console.log(image)
                     }
                   }
                 },
@@ -67,16 +63,12 @@
         } );
     
         nftElement[i].addEventListener('markerLost', (event) => {
-
-            //const image = document.querySelector('#nowimage')
             const model = document.querySelector('#nowmodel')
-            //const model = document.querySelector('#line')
-
+            const image = document.querySelector('#nowimage')
             const scene = document.querySelector('a-scene')
             scene.removeChild(model)
-            //scene.removeChild(image)
-        
-          })
+            scene.removeChild(image)
+        })
     
     }
 
